c0402d311be9b1bd3e84a3984988998554261dc8
Erkan Ozhan <52992248+erkanozhan@users.noreply.github.com>
2025-05-23 15:05:44 +0300

    New example

---
+++ Uygulamalar/Bash_App/3-Dosya_Okuma.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -f "deneme.txt" ]]; then
+	echo "Dosya var"
+	
+	while IFS= read -r okudu_sey; 
+	do
+	echo "Okunan Satır:$okudu_sey"
+	done < "deneme.txt"
+else
+echo "Dosya bulunamadı"
+fi
+
+## Bash `IFS` (Internal Field Separator) Nedir?
+
+# `IFS`, yani **Internal Field Separator**, Bash kabuğunda veri ayrıştırmak için kullanılan **özel bir değişkendir**. Genellikle `read`, `for`, `set`, ve `awk` gibi komutlarla kullanılırken satır veya kelime bazlı veri işlemede alanların nasıl bölüneceğini belirler.
+
+
+# `IFS` değişkeni, birden fazla karakter içerebilir ve bu karakterler, veri ayrıştırma işlemi sırasında alanları ayırmak için kullanılır. Örneğin, boşluk, tab veya yeni satır gibi karakterler `IFS` değişkeninde tanımlanabilir.
+## Varsayılan IFS
+
+# Varsayılan olarak Bash’te `IFS` aşağıdaki karakterleri içerir:
+
+# IFS=" \t\n"
