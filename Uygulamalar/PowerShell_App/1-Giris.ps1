b1597749bcb41f152b1ec9e488da9f223282619d
Erkan Ozhan <52992248+erkanozhan@users.noreply.github.com>
2025-05-23 20:30:07 +0300

    new explain

--- Uygulamalar/PowerShell_App/1-Giris.ps1
+++ Uygulamalar/PowerShell_App/1-Giris.ps1
@@ -1,7 +1,11 @@
 $name=Read-Host "Adınızı Girin"
-$age=Read-Host "Yaşınızı Girin"
+[int]$age=Read-Host "Yaşınızı Girin"
 if ($age -lt 18) {
 Write-Output "Çocuk"
 } else {
     Write-Output "Yetişkin"
 }
+
+# PowerShell'i Yönetici olarak çalıştırıp, aşağıdaki komutları çalıştırırsanız,
+# güvenlik ayarlarını değiştirebilirsiniz.
+# `Set-ExecutionPolicy RemoteSigned -Scope CurrentUser`
