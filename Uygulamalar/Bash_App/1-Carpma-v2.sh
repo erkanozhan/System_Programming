2219f9418a311aa7443600503b60e6856d66fd1d
Erkan Ozhan <erkanozhan@gmail.com>
2026-05-04 19:00:22 +0300

    Add detailed comments for input validation in multiplication script

--- Uygulamalar/Bash_App/1-Carpma-v2.sh
+++ Uygulamalar/Bash_App/1-Carpma-v2.sh
@@ -9,6 +9,15 @@ do
  read -p "İkinci sayıyı giriniz: " num2
 
 if ! [[ "$num1" =~ ^-?[0-9]+(\.[0-9]+)?$ && "$num2" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+# Girilen değerler sayı değilse hata mesajı 
+# gösterilir ve kullanıcıdan devam etmek için bir tuşa basması istenir.
+# Burada ~= operatörünü kullanarak regex ile sayısal değerleri 
+# kontrol ediyoruz.
+# ^-? : Sayının başında isteğe bağlı olarak negatif işareti olabilir.
+# [0-9]+ : Bir veya daha fazla rakam içermelidir.
+# (\.[0-9]+)? : Ondalık kısmı isteğe bağlı olarak içerebilir, 
+# ancak varsa bir nokta ve ardından bir veya daha fazla rakam içermelidir.
+
 echo "Lütfen sadece sayı giriniz. Ondalık ayraç olarak+
 virgül yerine . kullanınız"
 read -n 1 -s -r -p "Devam etmek için bir tuşa basınız"
